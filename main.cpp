b7a601c4e0ea037b97619659372131e9c2fedc27
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2025-02-17 02:33:19 +0900

    refactor: main.cpp include

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include "DxLib.h"
-#include <memory>
 #include "Application.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
